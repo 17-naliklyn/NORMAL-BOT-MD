d91b8e05c86834e344b3d54ffebb5fc8ecaa5ec6
17-naliklyn <chombakimuhu@gmail.com>
2025-03-04 21:12:40 +0300

    Update Reboot.js

--- commandes/Reboot.js
+++ commandes/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("POPKID-MD bot Restarting ⚽");
+    repondre("NALIKLYN bot Restarting ⚽");
 
   exec("pm2 restart all");
   
